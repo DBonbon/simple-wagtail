8de558bb32312e7775f8f0de9dc74f14c0563313
dbonbon <yrfriedman@gmail.com>
2023-05-09 21:02:44 +0200

    prod file

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,10 @@
 from .base import *
 
 DEBUG = True
+SECRET_KEY = "django-insecure-yue(b!9h^a+d(ue57q2)pu+)bb(h3a0r-ta(q^q_!-sy=2r_oe"
+ALLOWED_HOSTS = ["*"]
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 try:
     from .local import *
 except ImportError:
